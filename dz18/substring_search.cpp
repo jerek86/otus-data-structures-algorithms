b971391a37bdda045e2ef8cc11d5e85f53629553
Stroev Vladimir <vastroev@sberbank.ru>
2024-04-21 13:18:39 +0300

    KMP

--- dz18/substring_search.cpp
+++ dz18/substring_search.cpp
@@ -67,6 +67,43 @@ public:
         return -1;
     }
 
+    int finite_state_machine_search(const string& text, const string& mask) {
+        init_FSM_delta(mask);
+        int q = 0;
+        for (int i = 0; i < text.length(); ++i) {
+            q = FSM_delta_[FSM_delta_index(q, text[i])];
+            if (q == mask.length()) {
+                return i - mask.length() + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int KMP(const string& text, const string& mask) {
+        string total = mask + "#" + text;
+        std::vector<int> pi(total.length() + 1);
+        pi[0] = 0;
+        pi[1] = 0;
+        for (int i = 1; i < total.length(); ++i) {
+            int len = pi[i];
+
+            while (len > 0 && total[len] != total[i]) {
+                len = pi[len];
+            }
+            if (total[len] == total[i]) {
+                ++len;
+            }
+            pi[i + 1] = len;
+            
+            if (len == mask.length()) {
+                return i - 2*mask.length();
+            }
+        }
+
+        return -1;
+    }
+
 private:
     void fill_BMH_shift(const string& mask) {
         for (size_t i = 0; i < 256; ++i) {
@@ -121,17 +158,67 @@ private:
     }
 
     std::vector<int> BM_shift_;
+
+
+    enum { ALPHABET_SIZE = 256 };
+    int FSM_delta_index(int mask_index, int symbol) {
+        return (mask_index * ALPHABET_SIZE) + symbol;
+    }
+    string left(const string& str, int k) {
+        return str.substr(0, k);
+    }
+    string right(const string& str, int k) {
+        return str.substr(str.length() - k);
+    }
+    void init_FSM_delta(const string& mask) {
+        FSM_delta_.resize(FSM_delta_index(mask.length(), 0), 0);
+        for (int q = 0; q < mask.length(); ++q) {
+            for (int i = 0; i < ALPHABET_SIZE; ++i) {
+                char c = static_cast<char>(i);
+                int k = q + 1;
+                if (q == mask.length()) {
+                    --k;
+                }
+                string line = left(mask, q) + c;
+                while (left(mask, k) != right(line, k) && k >= 0) {
+                    --k;
+                }
+                FSM_delta_[FSM_delta_index(q, i)] = k;
+            }
+        }
+    }
+    
+    std::vector<int> FSM_delta_;
+
+
+    void init_KMP_pi(const string& mask) {
+        KMP_pi_.resize(mask.length() + 1);
+        KMP_pi_[0] = 0;
+        KMP_pi_[1] = 0;
+        for (int i = 1; i < mask.length(); ++i) {
+            int len = KMP_pi_[i];
+            while (len > 0 && mask[len] != mask[i]) {
+                len = KMP_pi_[len];
+            }
+            if (len > 0) {
+                ++len;
+            }
+            KMP_pi_[i + 1] = len;
+        }
+    }
+    
+    std::vector<int> KMP_pi_;
 };
 
 
 void test(const string& text, const string& mask, size_t count) {
-    int result = SubstringSearch{}.BMH(text, mask);
+    int result = SubstringSearch{}.KMP(text, mask);
     cout << "'" << mask << "'" << " starts from symbol " << result << " in " << "'" << text << "'" << endl;
 
     auto start = high_resolution_clock::now();
 
     for (size_t j = 0; j < count; j++) {
-        SubstringSearch{}.BMH(text, mask);
+        SubstringSearch{}.KMP(text, mask);
     }
 
 	auto stop = high_resolution_clock::now();
@@ -143,7 +230,8 @@ void test(const string& text, const string& mask, size_t count) {
 int main() {
 
     // test("data structures and algorithms", "rithm", 10000000);
-    test("abeccaabadbabbaabeccaabadbabbaabeccaabadbabbaabeccaabadbabbaabeccaabadbabbaabeccaabadbabbad", "abadbabbad", 10000000);
+    test("abeccaabadbabbad", "abbad", 10000000);
+    // test("abeccaabadbabbaabeccaabadbabbaabeccaabadbabbaabeccaabadbabbaabeccaabadbabbaabeccaabadbabbad", "abadbabbad", 10000000);
 
     return 0;
 }
